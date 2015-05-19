8304f8c921578a57a9fa0394e9d9449bf3e2b25e
Krishna Patel <softeng.krishna@gmail.com>
2015-05-19 20:56:56 +0530

    qdscx

--- app/src/main/java/sac/app/com/shoppingappclub/SplashPage.java
+++ app/src/main/java/sac/app/com/shoppingappclub/SplashPage.java
@@ -1,12 +1,15 @@
 package sac.app.com.shoppingappclub;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.util.Log;
+import android.widget.Toast;
 
 public class SplashPage extends ActionBarActivity {
 
@@ -14,6 +17,34 @@ public class SplashPage extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(checkInternet()){
+
+             new CountDownTimer(4000,4000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    Intent i  = new Intent(SplashPage.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }.start();
+
+        }else{
+            Toast.makeText(SplashPage.this,"Please connect your Internet",Toast.LENGTH_LONG).show();
+        }
     }
 
     private boolean checkInternet(){

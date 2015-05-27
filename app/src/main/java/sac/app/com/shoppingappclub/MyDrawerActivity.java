a1dae5226de17e91a9e5ba0b650d8c1500bf185e
Nirav <niravnvk@gmail.com>
2015-05-27 20:24:42 +0530

    dsd

--- app/src/main/java/sac/app/com/shoppingappclub/MyDrawerActivity.java
+++ app/src/main/java/sac/app/com/shoppingappclub/MyDrawerActivity.java
@@ -94,6 +94,7 @@ public class MyDrawerActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setBackgroundColor(Color.parseColor("#494949"));
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
         navigationDrawerAdapter = new ArrayAdapter<String>(MyDrawerActivity.this, android.R.layout.simple_list_item_activated_1, android.R.id.text1, leftSliderData);
         leftDrawerList.setAdapter(new lViewadapter());
 
